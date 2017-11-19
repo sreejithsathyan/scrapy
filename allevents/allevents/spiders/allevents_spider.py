37b3198fdf08a6d7341549c3dded44f278008e80
sreejith <sreejithsathyan95@gmail.com>
2017-11-19 21:35:18 +0530

    second

--- allevents/allevents/spiders/allevents_spider.py
+++ allevents/allevents/spiders/allevents_spider.py
@@ -5,19 +5,28 @@ from allevents.items import AlleventsItem
 class AlleventsSpiderSpider(scrapy.Spider):
     name = 'allevents_spider'
     allowed_domains = ['https://allevents.in']
-    start_urls = ['https://allevents.in/new%20delhi/expressionism-art-workshop-2017/80006190664762','https://allevents.in/noida/carnival-de-cuisine/80001719226909']
+    start_urls = ['https://allevents.in/new%20delhi/train-the-trainer-workshop/2044484635787135']
 
     def parse(self, response):
         title = response.xpath('//h1[@class="overlay-h1"]//text()').extract()
-	print title
+	#print title
 	time = response.xpath('//ul[@class="meta-list"]//text()').extract()[2].split('\t')[12]
-	print time
+	#print time
 	address = response.xpath('//ul[@class="meta-list"]//text()').extract()[33].split('\t')[6]
-	print address
+	#print address
+	organizer = response.xpath('//ul[@class="meta-list"]//text()').extract()[40]
+	#print organizer
+	followers = response.xpath('//div[@class="detail"]//text()').extract()[3]
+	#print folowers_count
+	event = response.xpath('//div[@class="detail"]//text()').extract()[10]
+	#print event_count
 	#Give the extracted content row wise
         output = {
-			"title":title,
-			"time":time,
-			"address":address
+			"Title":title,
+			"Time":time,
+			"Address":address,
+			"Organizer" :organizer,
+			"followers_count" :followers,
+			"event_count" :event
 		}
 	yield output
